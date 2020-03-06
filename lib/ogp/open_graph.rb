e38fa12b013fc2cdf96f1e4ba07277aa03b91ddd
Hazemturki <hazem@ibtmena.com>
2020-03-06 14:24:08 +0200

    Return all ogp attributes

--- lib/ogp/open_graph.rb
+++ lib/ogp/open_graph.rb
@@ -4,16 +4,7 @@ REQUIRED_ATTRIBUTES = %w(title type image url).freeze
 
 module OGP
   class OpenGraph
-    # Required Accessors
-    attr_accessor :title, :type, :url
-    attr_accessor :images
-    attr_accessor :source
-
-    # Optional Accessors
-    attr_accessor :description, :determiner, :site_name
-    attr_accessor :audios
-    attr_accessor :locales
-    attr_accessor :videos
+    attr_accessor :source, :attributes
 
     def initialize(source)
       body = source.try(:body)
@@ -26,12 +17,8 @@ module OGP
       body.force_encoding('UTF-8') if body.encoding != 'UTF-8'
 
       self.source = source
-      self.audios = []
-      self.locales = []
-      self.videos = []
-      self.images = []
       document = Nokogiri::HTML::Document.parse(body)
-      parse_attributes(document)
+      self.attributes = parse_attributes(document)
     end
 
     def image
@@ -55,39 +42,15 @@ module OGP
 
     # rubocop:disable Metrics/CyclomaticComplexity
     def parse_attributes(document)
-      document.xpath('//head/meta[starts-with(@property, \'og:\')]').each do |attribute|
-        attribute_name = attribute['property'].downcase.gsub('og:', '')
-        case attribute_name
-          when /^image$/i
-            images << OpenStruct.new(url: attribute['content'].to_s)
-          when /^image:(.+)/i
-            images << OpenStruct.new unless images.last
-            images.last[Regexp.last_match[1].gsub('-', '_')] = attribute['content'].to_s
-          when /^audio$/i
-            audios << OpenStruct.new(url: attribute['content'].to_s)
-          when /^audio:(.+)/i
-            audios << OpenStruct.new unless audios.last
-            audios.last[Regexp.last_match[1].gsub('-', '_')] = attribute['content'].to_s
-          when /^locale/i
-            locales << attribute['content'].to_s
-          when /^video$/i
-            videos << OpenStruct.new(url: attribute['content'].to_s)
-          when /^video:(.+)/i
-            videos << OpenStruct.new unless videos.last
-            videos.last[Regexp.last_match[1].gsub('-', '_')] = attribute['content'].to_s
-          else
-            instance_variable_set("@#{attribute_name}", attribute['content'].to_s)
-        end
+      hash = {}
+      keys = document.xpath('//head/meta[starts-with(@property, \'og:\')]').map{|tag| tag.attributes['property'].value }.uniq
+      keys.each do |key|
+        hash[key.tr(":", "_").to_sym] = document.xpath("//*[@property='#{key}']").map{|meta_og| meta_og.attributes['content'].value}
       end
-      if self.title.blank?
-        self.title=document.title
-      end
-      if self.description.blank?
-        self.description=document.at('meta[name="description"]').try(:[], 'content')
-      end
-      if self.url.blank?
-        self.url = source.request.uri.to_s
-      end
+      hash[:html_title]=document.title
+      hash[:html_description]=document.at('meta[name="description"]').try(:[], 'content')
+      hash[:html_url] = source.request.uri.to_s
+      hash
     end
 
     def attribute_exists(document, name)
